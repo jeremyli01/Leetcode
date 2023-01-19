3fa3f15b7eeab9ee9b479f5e208841799e915132
jeremyli01 <zihanli.personal@gmail.com>
2023-01-19 10:57:35 -0500

    Understand how to construct binary search tree from inorder and preorder array

---
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int preorderIndex;
+    Map<Integer, Integer> inorderMap;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preorderIndex = 0;
+        inorderMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+        return arrayToTree(preorder, 0, preorder.length - 1);
+    }
+    private TreeNode arrayToTree(int[] preorder, int left, int right) {
+        if (left > right) return null;
+
+        int rootValue = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootValue);
+
+        root.left = arrayToTree(preorder, left, inorderMap.get(rootValue) - 1);
+        root.right = arrayToTree(preorder, inorderMap.get(rootValue) + 1, right);
+        return root;
+    }
+}
